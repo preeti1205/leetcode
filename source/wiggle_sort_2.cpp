55a4965f67011299a5842faed8e35acd282e7c3c
preeti1205 <preeti.bhandari1205@gmail.com>
2016-09-07 18:47:30 -0400

    wiggle

--- source/wiggle_sort_2.cpp
+++ source/wiggle_sort_2.cpp
@@ -1,5 +1,32 @@
+class Solution {
+public:
+void wiggleSort(vector<int>& nums) {
+    int n = nums.size();
+
+    // Find a median.
+    auto midptr = nums.begin() + n / 2;
+    nth_element(nums.begin(), midptr, nums.end());
+    int mid = *midptr;
+
+    // Index-rewiring.
+    #define A(i) nums[(1+2*(i)) % (n|1)]
+
+    // 3-way-partition-to-wiggly in O(n) time with O(1) space.
+    int i = 0, j = 0, k = n - 1;
+    while (j <= k) {
+        if (A(j) > mid)
+            swap(A(i++), A(j++));
+        else if (A(j) < mid)
+            swap(A(j), A(k--));
+        else
+            j++;
+    }
+}
+};
 //116 ms solution - non linear for sort - O(nlogn) solution
 /*
+[6 5 5 4] -> [6 5] [4 3] -> [4 6 3 5]
+[6 5  5 4 3] -> [6 5] [5] [4 3] -> [5 6 4 5 3]   -> [4 6 3 5 5]
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
