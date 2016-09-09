37efe3425cd73b1109c8fbf9c152af6db1969cdd
preeti1205 <preeti.bhandari1205@gmail.com>
2016-09-09 13:49:01 -0400

    pointers

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # LeetCode
 
-![Status](https://img.shields.io/badge/status-50%2F329-brightgreen.svg)
+![Status](https://img.shields.io/badge/status-51%2F329-brightgreen.svg)
 
 ##### Summary
 
 | Level  | Count|
 |--------|------|
 | Easy   |  22  |
-| Medium |  24  |
+| Medium |  25  |
 | Hard   |  4   |
--- source/new_functions.cpp
+++ source/new_functions.cpp
@@ -1,5 +1,4 @@
-nth element function in cpp: used to sort in O(n) time using some partial sorting algo - <algorithm>
-The element pointed at by nth is changed to whatever element would occur in that position if [first, last)
+****The element pointed at by nth is changed to whatever element would occur in that position if [first, last)
 was sorted.
 All of the elements before this new nth element are less than or equal to the elements after
 the new nth element.
@@ -11,3 +10,11 @@ the new nth element.
     std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
 
     nth_element is a partial sorting algorithm that rearranges elements in [first, last) such that:
+
+*****to arrange a vector in dec order : sort(res.begin(), res.end(), greater<int>())
+                                  sort(res.rbegin(), res.rend())
+
+**** upper_index gives a reference to the position that is greater than the given number
+lower_index gives a reference to the first position that is less than or equal to the given number
+
+****upper_bound(row.begin(), row.end(), num) - row.begin(); will give diffrence in position and not the value.
